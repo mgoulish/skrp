3daf042516e74a82f64542fd68ae1d503671a0b5
mick <mgoulish@redhat.com>
2025-11-08 04:23:11 -0500

    A new dimension added: router threads.  And 2D gnuplot graphs.

--- tests/throughput/2_routers/process_results.py
+++ tests/throughput/2_routers/process_results.py
@@ -10,8 +10,13 @@ def new_cpu ( cpu_values_set ) :
 
 
 
-def new_threads ( threads_values_set ) :
-  return dict.fromkeys ( threads_values_set, None )
+def new_sender_threads ( sender_threads_values_set ) :
+  return dict.fromkeys ( sender_threads_values_set, None )
+
+
+
+def new_router_threads ( sender_threads_values_set ) :
+  return dict.fromkeys ( sender_threads_values_set, None )
 
 
 
@@ -28,13 +33,13 @@ def new_test_result ( ) :
 
 
 
-# Needs to know the number of threads, because 
+# Needs to know the number of sender-threads, because 
 # that affects how the throughput value in the file
 # can be found.
-def find_throughput_from_file ( file_path, n_threads ) :
+def find_throughput_from_file ( file_path, n_sender_threads ) :
   val   = 0.0
   units = None
-  if n_threads == '1' :
+  if n_sender_threads == '1' :
     with open(file_path, 'r') as file:
       for line in file:
         if 'receiver' in line :
@@ -56,46 +61,6 @@ def find_throughput_from_file ( file_path, n_threads ) :
 
 
 
-def make_gnuplot_file ( CPUs, RESULTS_ROOT, ROUTER_VERSION ) :
-  file_path = f"{RESULTS_ROOT}/graphs/gplot"
-  print ( f"make_gnuplot_file: file: {file_path}" )
-  with open ( file_path, 'w') as f :
-    f.write("# Read command line args\n")
-    f.write(f"set title \"Throughput vs Per-Router CPU Allocation\\n Router Version {ROUTER_VERSION}\" font \",24\"\n")
-    f.write('set autoscale\n')
-    f.write('unset key\n')
-    f.write('set xlabel "Iperf3 Sender Threads"  font ",20"\n')
-    f.write('set xrange [ 0 : 11 ]\n')
-    f.write('set ylabel "Throughput (Gbits/sec)"  font ",20"\n')
-    f.write('set yrange [ 0 : 20 ]\n')
-    f.write('set ytics 0, 2\n')
-    f.write('set terminal jpeg size 1000, 1000\n')
-    f.write(f'set output "{RESULTS_ROOT}/graphs/all_cpus.jpg"\n')
-    f.write('plot ')
-    colors = ['red', 'orange', 'gold', 'greenyellow', 'dark-turquoise', 
-              'forest-green', 'blue', 'dark-violet', 'gray40', 'gray0' ]
-    count = 1
-    for CPU in CPUs:
-      color_number = count - 1
-      if color_number > len(colors) - 1 :
-        color_number = len(colors) - 1
-      color = colors[color_number]
-      f.write ( f'"{RESULTS_ROOT}/data/cpu_{CPU}.data" u (last_x{count}=$1):(last_y{count}=$2) with linespoints lt rgb "{color}" lw 3 pt 7 ps 2, \\\n' )
-      count += 1
-    
-    count = 1
-    for CPU in CPUs :
-      color_number = count - 1
-      if color_number > len(colors) - 1 :
-        color_number = len(colors) - 1
-      f.write ( f"'+' u (last_x{count}):(last_y{count}):(\"{CPU}%\") every ::0::0 w labels offset -1,1 tc rgb \"{colors[color_number]}\" font \",16\", \\\n" )
-      count += 1
-      
-
-
-
-    
-
 
 #==================================================
 # main 
@@ -109,81 +74,173 @@ ROUTER_VERSION    = sys.argv[1]
 TEST              = 'throughput'
 RESULTS_ROOT      = f"../../../results/{ROUTER_VERSION}/{TEST}"
 TEST_RESULTS_DIR  = f"{RESULTS_ROOT}/test_results"
-DATA_DIR          = f"{RESULTS_ROOT}/data"
+GRAPH_DIR         = f"{RESULTS_ROOT}/graphs"
 
-cpus_set       = set()
-threads_set    = set()
-iterations_set = set()
+cpus_set            = set()
+sender_threads_set  = set()
+router_threads_set  = set()
+iterations_set      = set()
+
+# Here is an example of the expected file name format:
+#   cpu_300_sender-threads_2_router-threads_4_iteration_1
+# components:
+#        0    cpu
+#        2    sender-threads
+#        4    router-threads
+#        6    iteration
 
 # Go through all the files to collect all
-# values of CPUs, threads, and iterations
+# values of CPUs, sender threads, router threads, and iterations.
 for file_name in os.listdir ( TEST_RESULTS_DIR ) :
   name_components = file_name.split('_')
-  if name_components[0] != 'cpu' or name_components[2] != 'threads' or name_components[4] != 'iteration' :
+  if name_components[0] != 'cpu'            or \
+     name_components[2] != 'sender-threads' or \
+     name_components[4] != 'router-threads' or \
+     name_components[6] != 'iteration' :
     print ( f"error parsing file name: {file_name}" )
     sys.exit(1)
 
-  cpu_val       = name_components[1]
-  threads_val   = name_components[3]
-  iteration_val = name_components[5]
+  cpu_val             = name_components[1]
+  sender_threads_val  = name_components[3]
+  router_threads_val  = name_components[5]
+  iteration_val       = name_components[7]
 
-  cpus_set.add       ( cpu_val )
-  threads_set.add    ( threads_val )
-  iterations_set.add ( iteration_val )
+  cpus_set.add            ( cpu_val )
+  sender_threads_set.add  ( sender_threads_val )
+  router_threads_set.add  ( router_threads_val )
+  iterations_set.add      ( iteration_val )
   print (f"parsed file {file_name}")
+
 
 # Make the dictionary tree that will hold all
 # the throughput values.
 CPUs = new_cpu ( cpus_set )
 for cpu_value in CPUs :
-  CPUs[cpu_value] = new_threads ( threads_set )
-  for threads_value in CPUs[cpu_value] :
-    CPUs[cpu_value][threads_value] = new_iterations ( iterations_set )
-    for iteration_value in CPUs[cpu_value][threads_value] :
-      CPUs[cpu_value][threads_value][iteration_value] = new_test_result ( )
+  sender_threads_dict = new_sender_threads ( sender_threads_set )
+  CPUs[cpu_value] = sender_threads_dict
+  for sender_threads_value in sender_threads_dict :
+    router_threads_dict = new_router_threads ( router_threads_set )
+    sender_threads_dict[sender_threads_value] = router_threads_dict
+    for router_threads_value in router_threads_dict :
+      iterations_dict = new_iterations ( iterations_set )
+      router_threads_dict[router_threads_value] = iterations_dict
+      for iteration_value in iterations_dict :
+        iterations_dict[iteration_value] = new_test_result ( )
+
 
 # Now go through all the files again to read
 # the throughput values.  The payload.
 for file_name in os.listdir ( TEST_RESULTS_DIR ) :
   file_path = os.path.join ( TEST_RESULTS_DIR, file_name )
   name_components = file_name.split('_')
-  if name_components[0] != 'cpu' or name_components[2] != 'threads' or name_components[4] != 'iteration' :
-    print ( f"error parsing file name: {file_name}" )
-    sys.exit(1)
+  # No need to confirm that file name is well-formed,
+  # since we just did that, above.
 
-  cpu_val       = name_components[1]
-  threads_val   = name_components[3]
-  iteration_val = name_components[5]
+  cpu_val             = name_components[1]
+  sender_threads_val  = name_components[3]
+  router_threads_val  = name_components[5]
+  iteration_val       = name_components[7]
 
-  throughput, units = find_throughput_from_file ( file_path, threads_val )
+  throughput, units = find_throughput_from_file ( file_path, sender_threads_val )
 
   if units != 'Gbits/sec' :
     print ( f"error: unknown units:{units} in file {file_name}" )
     sys.exit ( 1 )
   
-  CPUs[cpu_val][threads_val][iteration_val]['throughput'] = throughput
-  CPUs[cpu_val][threads_val][iteration_val]['units']      = units
+  CPUs[cpu_val][sender_threads_val][router_threads_val][iteration_val]['throughput'] = throughput
+  CPUs[cpu_val][sender_threads_val][router_threads_val][iteration_val]['units']      = units
 
+#----------------------------------------------------------
 # Walk through the dictionary tree to get down to 
 # the test iterations, and find and store their averages.
+#----------------------------------------------------------
 for cpu_val in CPUs :
-  for threads_val in CPUs[cpu_val] :
-    throughput_sum = 0.0
-    for iteration_val in CPUs[cpu_val][threads_val] :
-      test_result = CPUs[cpu_val][threads_val][iteration_val]
-      throughput_sum += test_result [ 'throughput' ]
-    throughput_avg = throughput_sum / len(CPUs[cpu_val][threads_val])
-    #print ( f"cpu {cpu_val}  threads {threads_val} : throughput: {throughput_avg:.2f} " )
-    CPUs[cpu_val][threads_val]['average'] = throughput_avg
+  sender_threads_dict = CPUs[cpu_val]
+  for sender_threads_val in sender_threads_dict :
+    router_threads_dict = sender_threads_dict[sender_threads_val]
+    for router_threads_val in router_threads_dict :
+      throughput_sum = 0.0
+      iteration_dict = router_threads_dict[router_threads_val]
+      for iteration_val in iteration_dict:
+        test_result_dict = iteration_dict[iteration_val]
+        throughput_sum += test_result_dict [ 'throughput' ]
+      throughput_avg = throughput_sum / len(iteration_dict)
+      # It's a little icky to change the iteraton dict like
+      # this, and overload it to store the average across iterations. 
+      # But. Who's gonna know?
+      iteration_dict['average'] = throughput_avg
 
 # Now make the data files that will be used by gnuplot for the graphs.
+#
+# Each data file will represent a different CPU allocation.
+# Gnuplot will create a 2-dimensional graph where:
+#    * X-axis shows number of router threads 
+#    * Y-axis shows the number of sender threads.
+#
+# Each data file should look something like this:
+#             10 15 20 25 30
+#             8 12 18 22 28
+#             5 10 15 20 25
+#             3 7 12 17 22
+#             1 5 10 15 20
+# There are no explicit Y values. Those are encoded implicitly
+# as the row numbers, where the top row will be at the top of the
+# graph.
+# There are also no explicit X values. Those are encoded implicitly
+# as the positions of each value in its line.
+# The numbers in the data file are just the throughput values.
+
 for cpu_val in CPUs :
-  data_file_path = f"{DATA_DIR}/cpu_{cpu_val}.data"
-  print ( f"writing data file: {data_file_path}" )
-  with open ( data_file_path, 'w') as f :
-    for threads_val, test_results in sorted(CPUs[cpu_val].items(), key=lambda item: int(item[0])):
-      f.write(f"{threads_val} {test_results['average']:.2f}\n")
+  gnuplot_script_file_path =   f"{GRAPH_DIR}/cpu_{cpu_val}.gplot"
+  gnuplot_data_file_path   =   f"{GRAPH_DIR}/cpu_{cpu_val}.data"
+  print ( f"writing script file: {gnuplot_script_file_path}  and data file: {gnuplot_data_file_path}" )
+  # Data file Y-axis is sender threads
+  sender_threads_dict = CPUs[cpu_val]
+  with open ( gnuplot_data_file_path, 'w' ) as f:
+    # Sender threads on the Y-axis
+    sorted_y_vals = sorted(sender_threads_dict.keys(), key=int)
+    for sender_threads_val in sorted_y_vals :
+      print ( f"sender_threads_val: {sender_threads_val}" )
+      # Router threads on the X-axis
+      sorted_x_vals = sorted ( router_threads_dict.keys(), key=int )
+      router_threads_dict = sender_threads_dict[sender_threads_val]
+      for router_threads_val in sorted_x_vals :
+        iteration_dict = router_threads_dict[router_threads_val]
+        f.write ( f"{iteration_dict['average']:.2f} ")
+      f.write ( "\n" )
 
+  # And now write the gnupplot script to the same directory.
+  gnuplot_image_file_path   =   f"{GRAPH_DIR}/cpu_{cpu_val}.jpg"
+  print ( f"writing gnuplot script {gnuplot_script_file_path}" )
+  with open ( gnuplot_script_file_path, 'w' ) as f :
+    f.write (  "set terminal png size 1200, 1000\n" )
+    f.write ( f"set output '{gnuplot_image_file_path}'\n" )
+    f.write (  "set palette rgbformulae 33,13,10\n" )
+    f.write (  "set colorbox\n" )
+    f.write ( f'set title "Throughput for CPU {cpu_val}" font ",24" \n' )
+    f.write (  'set xlabel "Router Threads" font ",20" \n' )
+    f.write (  'set ylabel "Sender Threads" font ",20" \n' )
 
-make_gnuplot_file ( sorted(CPUs.keys(), key=int, reverse=True), RESULTS_ROOT, ROUTER_VERSION )
+    # X Tics -----------------
+    f.write (  "set xtics font \",16\" (" )
+    count = 0
+    for x in sorted_x_vals :
+      f.write ( f'"{x}" {count}, ' )
+      count += 1
+    f.write (  ")\n" )
+
+    # Y Tics -----------------
+    f.write (  "set ytics font \",16\" (" )
+    count = 0
+    for y in sorted_y_vals :
+      f.write ( f'"{y}" {count}, ' )
+      count += 1
+    f.write (  ")\n" )
+
+    f.write (  "set autoscale xfix\n" )
+    f.write (  "set autoscale yfix\n" )
+    f.write (  "set autoscale cbfix\n" )
+    f.write ( f"plot '{gnuplot_data_file_path}' matrix with image title \"\", \\\n" )
+    f.write (  "     \"\"     matrix using 1:2:(sprintf('%.1f', $3)) with labels center font \",16\"" )
+    
 
